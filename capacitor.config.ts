5c6e82b0563e364a28b4482cae263491c0c0af4a
kamzago35 <kamzago2005@gmail.com>
2026-06-26 15:17:49 +0200

    first preparations

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'przystanki-v2',
+  webDir: 'dist/przystanki-v2/browser'
+};
+
+export default config;
