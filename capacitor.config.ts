3685cfb6d41a41459d817ac1e34a6508179300ca
kamzago35 <kamzago2005@gmail.com>
2026-06-27 00:46:55 +0200

    new icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'przystanki-v2',
+  appName: 'Przystanki V2',
   webDir: 'dist/przystanki-v2/browser'
 };
 
