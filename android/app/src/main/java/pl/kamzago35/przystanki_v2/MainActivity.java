c67aa5d763ae80866cd4cdb82c0cb577d54a6f2c
kamzago35 <kamzago2005@gmail.com>
2026-06-28 01:32:35 +0200

    new android build

--- android/app/src/main/java/pl/kamzago35/przystanki_v2/MainActivity.java
+++ android/app/src/main/java/pl/kamzago35/przystanki_v2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package pl.kamzago35.przystanki_v2;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'pl.kamzago35.przystanki_v2',
   appName: 'Przystanki V2',
   webDir: 'dist/przystanki-v2/browser'
 };
