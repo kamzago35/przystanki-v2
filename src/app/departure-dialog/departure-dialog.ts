55905b698e11ec146badbc48544e4c6cf912019d
kamzago35 <kamzago2005@gmail.com>
2026-06-28 23:32:11 +0200

    added info about departure

--- src/app/departure-dialog/departure-dialog.ts
+++ src/app/departure-dialog/departure-dialog.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, MatDialog } from '@angular/material/dialog';
 import { BusStop } from '../models/bus-stop-model';
 import { Departure } from '../models/departure-model';
 import { DeparturesService } from '../services/departures-service';
 import { ToastService } from '../services/toast-service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { DepartureInfoDialog } from '../departure-info-dialog/departure-info-dialog';
 
 @Component({
   selector: 'app-departure-dialog',
@@ -17,6 +18,7 @@ export class DepartureDialog implements OnInit {
   data = inject<{busStop: BusStop}>(MAT_DIALOG_DATA)
   private departuresService = inject(DeparturesService)
   private toastService = inject(ToastService)
+  private dialog = inject(MatDialog)
   departures = signal<Departure[]>([])
   isLoading = signal(true)
 
@@ -38,5 +40,9 @@ export class DepartureDialog implements OnInit {
       hour: '2-digit',
       minute: '2-digit'
     })
+  }
+
+  openDepartureInfoDialog(departure: Departure) {
+    this.dialog.open(DepartureInfoDialog, {data: {departure: departure}})
   }
 }
---
+++ src/app/departure-info-dialog/departure-info-dialog.ts
@@ -0,0 +1,52 @@
+import { Component, inject } from '@angular/core';
+import { MatDialogTitle, MatDialogContent, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Departure } from '../models/departure-model';
+
+@Component({
+  selector: 'app-departure-info-dialog',
+  imports: [MatDialogTitle, MatDialogContent],
+  templateUrl: './departure-info-dialog.html',
+  styleUrl: './departure-info-dialog.scss',
+})
+export class DepartureInfoDialog {
+  data = inject<{departure: Departure}>(MAT_DIALOG_DATA)
+
+  getType() {
+    switch(this.data.departure.line.type) {
+      case 'DAY': return 'dzienna'
+      case 'NIGHT': return 'nocna'
+      default: return 'Brak informacji'        
+    }
+  }
+
+  getSubtype() {
+    switch(this.data.departure.line.subtype) {
+      case 'NORMAL': return 'zwykła'
+      case 'SEMI_FAST': return 'przyspieszona'
+      case 'FAST': return 'pospieszna'
+      case 'REPLACEMENT': return 'zastępcza'
+      case 'ADDITIONAL': return 'dodatkowa'
+      case 'SPECIAL': return 'specjalna'
+      case 'TOURIST': return 'turystyczna'
+      default: return 'Brak informacji'
+    }
+  }
+
+  getVehicleType() {
+    switch(this.data.departure.line.vehicle_type) {
+      case 'BUS': return 'autobus'
+      case 'TRAM': return 'tramwaj'
+      case 'SKM': return 'szybka kolej miejska'
+      default: return 'Brak informacji'
+    }
+  }
+
+  getAccessibility() {
+    switch(this.data.departure.trip.accessibility) {
+      case 'HIGH_FLOOR': return 'Zaplanowany pojazd wysokopodłogowy'
+      case 'LOW_FLOOR': return 'Zaplanowany pojazd niskopodłogowy'
+      case 'LOW_FLOOR_POSSIBLE': return 'Możliwy pojazd niskopodłogowy'
+      default: return 'Brak informacji'
+    }
+  }
+}
